6d5387f54c6bd3f00a4a0a47711dca392de5b481
Lezrith <krzysztof.g.tomczak@student.put.poznan.pl>
2015-03-09 20:49:36 +0100

    Highscores fix V0.57d

--- Assets/Code/DisplayScore.cs
+++ Assets/Code/DisplayScore.cs
@@ -9,11 +9,11 @@ public class DisplayScore : MonoBehaviour
     public void Start()
     {
         score = new Text[5];
-        score[0] = GameObject.Find("High Scores/Scores/Text0").GetComponent<Text>();
-        score[1] = GameObject.Find("High Scores/Scores/Text1").GetComponent<Text>();
-        score[2] = GameObject.Find("High Scores/Scores/Text2").GetComponent<Text>();
-        score[3] = GameObject.Find("High Scores/Scores/Text3").GetComponent<Text>();
-        score[4] = GameObject.Find("High Scores/Scores/Text4").GetComponent<Text>();
+        score[0] = GameObject.Find("High Scores/Scores/Score1/Text").GetComponent<Text>();
+        score[1] = GameObject.Find("High Scores/Scores/Score2/Text").GetComponent<Text>();
+        score[2] = GameObject.Find("High Scores/Scores/Score3/Text").GetComponent<Text>();
+        score[3] = GameObject.Find("High Scores/Scores/Score4/Text").GetComponent<Text>();
+        score[4] = GameObject.Find("High Scores/Scores/Score5/Text").GetComponent<Text>();
         score[0].text = PlayerPrefs.GetInt("Wynik1").ToString();
         score[1].text = PlayerPrefs.GetInt("Wynik2").ToString();
         score[2].text = PlayerPrefs.GetInt("Wynik3").ToString();
